1584c9f1655dafe841d3a490ae579b8a89c5a4dc
Lucas <lugablima@gmail.com>
2023-01-29 15:25:51 -0300

    feat: add Page and Logo components

---
+++ front/src/layouts/Page.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Page = styled.div`
+  width: 100%;
+  max-width: 375px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default Page;
