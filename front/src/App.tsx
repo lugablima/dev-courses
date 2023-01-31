e878b818de8caedb6dbcf36bce00e461c7d630ba
Lucas <lugablima@gmail.com>
2023-01-30 22:52:37 -0300

    feat: update the app component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Header from "./components/Header";
+import ReloadProvider from "./contexts/ReloadContext";
 import UserProvider from "./contexts/UserContext";
+import AddCourse from "./pages/AddCourse";
+import EditCourse from "./pages/EditCourse";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -8,13 +12,18 @@ import SignUp from "./pages/SignUp";
 function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
+      <ReloadProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/add-course" element={<AddCourse />} />
+            <Route path="/edit-course/:courseId" element={<EditCourse />} />
+          </Routes>
+        </BrowserRouter>
+      </ReloadProvider>
     </UserProvider>
   );
 }
